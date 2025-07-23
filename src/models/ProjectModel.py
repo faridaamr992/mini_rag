fe43606337d526c7453e848782a3995a57854f36
faridaamr992 <faridaamr992@yahoo.com>
2025-07-23 12:49:41 +0300

    created assets collection

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -33,7 +33,7 @@ class ProjectModel(BaseDataModel):
 
         result= await self.collection.insert_one(project.dict())
         project.id = result.inserted_id
-        return result.inserted_id
+        return project
     
     async def get_project_or_create_one(self, project_id: str):
 
