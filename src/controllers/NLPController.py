37f4415cc970998d745aba803711e68dfd64ba64
faridaamr992 <faridaamr992@yahoo.com>
2025-07-29 13:52:03 +0300

    semantic search

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -3,6 +3,7 @@ from models.db_schemes import Project, DataChunk
 from stores.llm.LLMEnums import DocumentTypeEnum
 from typing import List
 import logging
+import json
 
 
 
@@ -26,7 +27,9 @@ class NLPController(BaseController):
     def get_vector_db_collection_info(self, project:Project):
         collection_name = self.create_collection_name(project_id=project.project_id)
         collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
-        return collection_info
+        return json.loads(
+            json.dumps(collection_info, default=lambda x:x.__dict__)
+        )
     
     def index_into_vector_db(self, project: Project, chunks: List[DataChunk],
                              chunks_ids: List[int],
@@ -59,3 +62,28 @@ class NLPController(BaseController):
             record_ids=chunks_ids,
         )
         return True
+    
+    def search_vector_db_collection(self, project: Project, text:str,limit: int = 10):
+
+        # step1 : get collection name
+        collection_name = self.create_collection_name(project_id=project.project_id)
+
+        # step2 : get text embedding vector 
+        vector = self.embedding_client.embed_text(text=text,document_type=DocumentTypeEnum.QUERY.value)
+
+        if not vector or len(vector) == 0:
+            return False
+        
+        # step3 : do semantic search 
+        results = self.vectordb_client.search_by_vector(
+            collection_name=collection_name,
+            vector=vector,
+            limit=limit
+        )
+        if not results:
+            return False
+
+        return json.loads(
+            json.dumps(results,default= lambda x: x.__dict__)
+        )
+
