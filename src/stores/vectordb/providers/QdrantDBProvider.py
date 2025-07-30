618e0dcc8e63e952c8d9558c789640b1f8953333
faridaamr992 <faridaamr992@yahoo.com>
2025-07-30 14:45:00 +0300

    generation

--- src/stores/vectordb/providers/QdrantDBProvider.py
+++ src/stores/vectordb/providers/QdrantDBProvider.py
@@ -4,6 +4,7 @@ from ..VectorDBEnums import DistanceMethodEnums
 from qdrant_client import models, QdrantClient
 import logging
 from typing import List,Optional,Any
+from models.db_schemes import RetrievedDocument
 
 class QdrantDBProvider(VectorDBInterface):
 
@@ -139,10 +140,22 @@ class QdrantDBProvider(VectorDBInterface):
         
     def search_by_vector(self, collection_name:str,vector:list,limit:int = 5):
 
-            return self.client.search(
+            results = self.client.search(
                 collection_name=collection_name,
                 query_vector=vector,
-                limit=limit)
+                limit=limit
+                )
+            if not results or len(results)==0:
+                return None
+            
+            return[
+
+                RetrievedDocument(**{
+                    "score": result.score,
+                    "text": result.payload["text"],
+                })
+                for result in results
+            ]
             
 
 
